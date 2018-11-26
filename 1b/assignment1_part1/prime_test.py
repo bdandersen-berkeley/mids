2772c5dea5a5583fa78cb734b317c667eae07489
Brad Andersen <bdandersen@berkeley.edu>
2018-11-25 18:04:27 -0800

    Completed Assignment 1, Part 1 - Answer verified

--- 1b/assignment1_part1/prime_test.py
+++ 1b/assignment1_part1/prime_test.py
@@ -13,7 +13,7 @@ def isPrime(n):
         if (cur_divisor == max_divisor):
             return True
         elif (0 == n % cur_divisor):
-            print("{0} is divisible by {1}".format(n, cur_divisor))
+            # print("{0} is divisible by {1}".format(n, cur_divisor))
             return False
         else:
             max_divisor = n // cur_divisor
@@ -21,8 +21,28 @@ def isPrime(n):
 
     return True
 
+def getLeastPrimeDivisor(n, primes):
+
+    if (n in primes):
+        return n
+
+    for prime in primes:
+        if (0 == n % prime):
+            return prime
+        
+    return n
+
 primes = set()
-for x in range(1, 2001):
+for x in range(1, 10000):
     if (isPrime(x)):
         primes.add(x)
-print(sorted(primes))
+
+n = 1234567890
+divisors = list()
+while (n not in primes):
+    leastPrimeDivisor = getLeastPrimeDivisor(n, sorted(primes))
+    divisors.append(leastPrimeDivisor)
+    n = n // leastPrimeDivisor
+divisors.append(n)
+
+print(divisors)
