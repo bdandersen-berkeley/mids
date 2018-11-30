48943da6bd8e21727e798858a0e7f27a7a0cdc28
Brad Andersen <bdandersen@berkeley.edu>
2018-11-29 17:06:59 -0800

    Initial implementation of single-digit summation (Assignment 1, Part 2)

---
+++ 1b/assignment1_part2/phone_number_calc.py
@@ -0,0 +1,21 @@
+def sumDigits(n):
+    sum = 0
+    modPair = divmod(n, 10)
+    while (modPair[0] != 0):
+        sum = sum + modPair[1]
+        modPair = divmod(modPair[0], 10)
+    return sum + modPair[1]
+
+inputIsInt = False
+while False == inputIsInt:
+    try:
+        phone = input("Please enter your phone number (without punctuation): ")
+        phone = int(phone)
+        inputIsInt = True
+    except:
+        print("Your phone number must consist only of digits - try again")
+
+y = phone - sumDigits(phone)
+while (y >= 10):
+    y = sumDigits(y)
+print("Answer: ", y)
