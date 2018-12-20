d8373574bece570a63fec03129008527d7c9d80e
Brad Andersen <bdandersen@berkeley.edu>
2018-12-19 16:38:12 -0800

    Completed

--- 1b/assignment2/winning_strategy.py
+++ 1b/assignment2/winning_strategy.py
@@ -1,17 +1,20 @@
-def playerOneHasWinningStrategy(n, isPlayerOne):
+def hasWinningStrategy(n, myPlay):
 
     if (n < 2):
-        print("Base condition reached; n: " + str(n) + ", Player one must lose: " + str(isPlayerOne))
-        return not isPlayerOne
+        print("n = " + str(n) + ", returning: " + str(not myPlay))
+        return not myPlay
 
-    print("n: " + str(n) + ", Player one's turn: " + str(isPlayerOne))
-    nPrime = n
-    return \
-        playerOneHasWinningStrategy(n - 1, not isPlayerOne) and \
-        playerOneHasWinningStrategy(nPrime / 2, not isPlayerOne)
+    leftStrategy = hasWinningStrategy(n - 1, not myPlay)
+    rightStrategy = hasWinningStrategy(n / 2, not myPlay)
+    print("n = " + str(n) + ", left strategy: " + str(leftStrategy) + ", right strategy: " + str(rightStrategy))
+    return leftStrategy or rightStrategy
 
 inputIsFloat = False
 while False == inputIsFloat:
+    firstMove = False
+    firstMoveEntry = input("Will Player One make the first move (T/F)? ")
+    if str(firstMoveEntry) is "T":
+        firstMove = True
     try:
         n = input("Please enter the beginning number: ")
         n = float(n)
@@ -19,4 +22,4 @@ while False == inputIsFloat:
     except:
         print("The beginning number must be a float - try again")
 
-print("Player One has winning strategy: " + str(playerOneHasWinningStrategy(n, True)))
+print("Player One has winning strategy: " + str(hasWinningStrategy(n, firstMove)))
