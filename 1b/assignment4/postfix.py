cb3a95031ab378fcfb7b56afca1631b6ee2e22ae
Brad Andersen <bdandersen@berkeley.edu>
2018-12-25 14:19:00 -0800

    Assignment 4 implementation for submission

--- 1b/assignment4/postfix.py
+++ 1b/assignment4/postfix.py
@@ -2,6 +2,8 @@ import fileinput
 import os
 import re
 
+# Element maintained in stack PostfixStack.
+# Represented by a value, and a reference to the next PostfixStackElem on the stack.
 class PostfixStackElem:
 
     def __init__(self, value):
@@ -17,6 +19,7 @@ class PostfixStackElem:
     def getValue(self):
         return self._value
 
+# Stack implementation for use with MIDS 1b Assignment 4 Postfix exercise.
 class PostfixStack:
 
     def __init__(self):
@@ -40,29 +43,41 @@ class PostfixStack:
         self._headElem = tempElem
         return value
 
+# Dictionary associating individual characters with simple arithmetic operations.
+# Arithmetic operations supported include addition, subtraction, multiplication and division.
 ops = {}
 ops["+"] = lambda l, r: l + r
 ops["-"] = lambda l, r: l - r
 ops["*"] = lambda l, r: l * r
 ops["/"] = lambda l, r: l / r
 
+# Regular expressions with which string tokens in the Postfix equation are compared.
+# String tokens are assumed to be either characters representing arithmetic operations (e.g. "+")
+# or integers.
 regexOps = re.compile(r"^([\+\-\*\/])$")
 regexInts = re.compile(r"^([\d]+)$")
 
+# Create the stack with which to evaluate the Postfix equation.
 ps = PostfixStack()
 
+# Iterate through the lines in input.txt.
+# Assumption is made that each line represents a Postfix equation.  Postfix equations are 
+# evaluated and their final values written to output.txt.
 with fileinput.input(files = ("input.txt")) as fin:
-    for line in fin:
-        for token in line.split():
-            m = regexInts.match(token)
-            if m is not None:
-                ps.push(int(m.group(1)))
-            else:
+    with open("output.txt", "w") as fout:
+        for line in fin:
+            # Split the line into string tokens
+            for token in line.split():
+                # Integer? Push it onto the stack.
+                m = regexInts.match(token)
+                if m is not None:
+                    ps.push(int(m.group(1)))
+                # Arithmetic operator? Evaluate it with values from the stack, and push the result
+                # back upon the stack.
                 m = regexOps.match(token)
                 if m is not None:
-                    l = ps.pop()
                     r = ps.pop()
+                    l = ps.pop()
                     ps.push(ops[m.group(1)](l, r))
-                else:
-                    print("Invalid syntax")
-        print(ps.pop())
+            # Write the value of the Postfix equation to the output file.
+            fout.write(format(ps.pop(), ".2f") + "\n")
