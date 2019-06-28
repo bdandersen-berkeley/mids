80268702f2f77204691312a30e29e71b1f3e971e
Brad Andersen <bdandersen@berkeley.edu>
2019-06-28 13:55:36 -0700

    Work for Questions 2d - 2f

--- W207/homework/p2.py
+++ W207/homework/p2.py
@@ -86,9 +86,40 @@ fraction_nonzero_entries = \
 print("Fraction of entries in matrix that are non-zero: %.4f" % fraction_nonzero_entries)
 
 q2c_vocabulary = ["atheism", "graphics", "space", "religion"]
-q2c_vectorizer = CountVectorizer(vocabulary =  ["atheism", "graphics", "space", "religion"])
+q2c_vectorizer = CountVectorizer(vocabulary =  q2c_vocabulary)
 q2c_term_matrix = q2c_vectorizer.fit_transform(newsgroups_train.data)
 fraction_nonzero_entries = \
     q2c_term_matrix.nnz / (q2c_term_matrix.shape[0] * q2c_term_matrix.shape[1])
 print(q2c_term_matrix.shape)
 print("Fraction of entries in matrix that are non-zero: %.4f" % fraction_nonzero_entries)
+
+# Initialize CountVectorizers with character ngram min and max lengths of (2, 2), (3, 3) and 
+# (2, 3), and identify the size of the resulting vocabularies once trained on the training data
+# for t in [(2, 2), (3, 3), (2, 3)]:
+for t in [(2, 2), (3, 3), (2, 3)]:
+    q2d_vectorizer = CountVectorizer(analyzer = "char", ngram_range = t)
+    q2d_term_matrix = q2d_vectorizer.fit_transform(newsgroups_train.data)
+    print("Character ngram limits:", t, "- Vocabulary size:", len(q2d_vectorizer.vocabulary_), "ngrams")
+
+# Create and train a vectorizer to prune words that appear in fewer than 10 documents 
+q2e_vectorizer = CountVectorizer(min_df = 10)
+q2e_term_matrix = q2e_vectorizer.fit_transform(newsgroups_train.data)
+
+# What size vocabulary does this yield?
+print("Minimum document frequency: 10 words - Vocabulary size:", len(q2e_vectorizer.vocabulary_), "words")
+
+q2f_train_vectorizer = CountVectorizer()
+q2f_dev_vectorizer = CountVectorizer()
+
+q2f_train_term_matrix = q2f_train_vectorizer.fit_transform(train_data)
+q2f_dev_term_matrix = q2f_dev_vectorizer.fit_transform(dev_data)
+
+missing_from_train_data = 0
+for w in q2f_dev_vectorizer.vocabulary_:
+    if w not in q2f_train_vectorizer.vocabulary_:
+        missing_from_train_data += 1 
+    else: 
+        pass
+
+fraction_missing_from_train_data = missing_from_train_data / len(q2f_dev_vectorizer.vocabulary_)
+print("Fraction of words in dev data missing from training vocabulary: %.4f" % fraction_missing_from_train_data)
